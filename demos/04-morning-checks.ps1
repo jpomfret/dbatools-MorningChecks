8516e594decc76319ca1e4ad6c52f6bf36624393
jpomfret <jpomfret7@gmail.com>
2025-06-18 16:35:04 +0000

    add error log

--- demos/04-morning-checks.ps1
+++ demos/04-morning-checks.ps1
@@ -10,14 +10,14 @@ $css = @"
 table.paleBlueRows {
     font-family: "Times New Roman", Times, serif;
     border: 1px solid #FFFFFF;
-    width: 350px;
+    
     height: 200px;
     text-align: center;
     border-collapse: collapse;
 }
 table.paleBlueRows td, table.paleBlueRows th {
     border: 1px solid #FFFFFF;
-    padding: 3px 2px;
+    padding: 4px 10px;
 }
 table.paleBlueRows tbody td {
     font-size: 13px;
@@ -86,6 +86,26 @@ if($backupIssues) {
     $body += ConvertTo-PSHTMLTable @table
 }
 
+# Error Log messages
+$errorLogMsgs = Get-DbaErrorLog -SqlInstance dbatools1 -LogNumber 0, 1 -After (get-date).AddDays(-1) | 
+Where-object { 
+        ($_.text  -like '*failed*') `
+    -or ($_.text  -like '*unable*') `
+    -or ($_.text  -like '*exception*') `
+    -or ($_.text  -like '*dragon*')`
+    -or ($_.text  -like '*critical*') 
+    } | Select-Object LogDate, text
+
+if($errorLogMsgs) {
+    $body += h2 { "Error Log Messages" }
+    $table = @{
+        Object = $errorLogMsgs
+        Properties = 'LogDate', 'text'
+        TableClass = 'paleBlueRows'
+    }
+    $body += ConvertTo-PSHTMLTable @table
+}
+
 # Query Store status
 $queryStoreStatus = Get-DbaDbQueryStoreOption -SqlInstance dbatools1, dbatools2 | 
 Where-Object { $_.ActualState -ne 'ReadWrite' } -WarningAction Ignore
@@ -103,6 +123,7 @@ if($queryStoreStatus) {
 $summary = ul {
     li { ("Databases Not in Expected State: {0}" -f $dbState.Count) }
     li { ("Databases with Backup Issues: {0}" -f $backupIssues.Count) }
+    li { ("Error Log Messages: {0}" -f $errorLogMsgs.Count) }
     li { ("Query Store Issues: {0}" -f $queryStoreStatus.Count) }
 }
 
